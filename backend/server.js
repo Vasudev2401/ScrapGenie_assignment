344722440c1cb12b7f85f477622acd621327f146
Vaibhav <vaibhavanandsingh24@gmail.com>
2025-01-19 16:33:38 +0530

    third message

--- backend/server.js
+++ backend/server.js
@@ -20,6 +20,22 @@ app.get('/',(req,res)=>{
     res.send(tasks)
 })
 
+app.put('/tasks/:id', (req, res) => {
+    const id = parseInt(req.params.id, 10); // Extract id from URL
+    const { status } = req.body; // Extract data from request body
+  
+    // Find the item by id
+    const taskIndex = tasks.findIndex((task) => task.id === id);
+  
+    if (taskIndex !== -1) {
+      // Update the item
+      tasks[taskIndex] = { ...tasks[taskIndex], status };
+      res.json({ message: 'Item updated successfully', item: items[itemIndex] });
+    } else {
+      // If the item doesn't exist
+      res.status(404).json({ message: 'Item not found' });
+    }
+  });
 
 app.listen(port,()=>{
     console.log(`Server is listening on port:${port}`);
